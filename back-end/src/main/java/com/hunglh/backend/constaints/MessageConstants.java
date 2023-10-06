22e78a9a9c1fb4c440021f0be8f1fef1b8dd8226
Le Ha Hung <hung_lh@flinters.vn>
2023-10-06 15:55:05 +0700

    add DTO

--- back-end/src/main/java/com/hunglh/backend/constaints/MessageConstants.java
+++ back-end/src/main/java/com/hunglh/backend/constaints/MessageConstants.java
@@ -4,4 +4,10 @@ public class MessageConstants {
     public static final String PRODUCT_NOT_FOUND = "Product not found";
     public static final String USER_NOT_FOUND = "User not found";
     public static final String EMAIL_NOT_FOUND = "Email not found";
+    public static final String INCORRECT_EMAIL = "Incorrect email";
+    public static final String EMAIL_CANNOT_BE_EMPTY = "Email cannot be empty";
+    public static final String EMPTY_FIRST_NAME = "First name cannot be empty";
+    public static final String EMPTY_LAST_NAME = "Last name cannot be empty";
+    public static final String PASSWORD_CHARACTER_LENGTH = "Password must be between 6 and 16 characters";
+    public static final String PASSWORD2_CHARACTER_LENGTH = "Password2 must be between 6 and 16 characters";
 }
